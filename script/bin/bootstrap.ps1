892eedc1607681e6980f2e94586b4f12d103348b
Pete O'Shea <mail@peteoshea.co.uk>
2019-08-11 16:48:31 +0100

    Allow for single chocolatey package
    Unexpectedly a single package gets handled as a string instead of a list and so getting the first item in the list gets the first letter from the string...
    Anyway pulled out into a function so the actual installation will not have to be duplicated.

--- script/bin/bootstrap.ps1
+++ script/bin/bootstrap.ps1
@@ -11,12 +11,22 @@ if ($LastExitCode) {
 }
 
 # Install required packages
+function Install-ChocolateyPackage {
+    param ([string]$Name)
+    Write-Host "===> Installing '$Name'"
+}
+
 $packagesFilePath = "$basePath\chocolatey-packages"
 if (Test-Path -Path "$packagesFilePath" -PathType Leaf) {
     Write-Host "==> Installing Chocolatey packages..."
     $packageList = Get-Content -Path "$packagesFilePath"
-    For ($index = 0; $index -lt $packageList.Count; $index++) {
-        $package = $packageList[$index];
-        Write-Host "===> Installing '$package'"
+    if ($packageList.Count -eq 1) {
+        # A single item is treated as a string not an array
+        Install-ChocolateyPackage -Name $packageList
+    } else {
+        For ($index = 0; $index -lt $packageList.Count; $index++) {
+            $package = $packageList[$index];
+            Install-ChocolateyPackage -Name $package
+        }
     }
 }
