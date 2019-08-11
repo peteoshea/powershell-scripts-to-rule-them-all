9fe9d323718dd0996a5d3e06a3f337b8f91db893
Pete O'Shea <mail@peteoshea.co.uk>
2019-08-11 13:19:24 +0100

    Process list of packages that need to be installed
    Also tweaked the variable names to be camelCase based on recommended practise.

--- script/bin/bootstrap.ps1
+++ script/bin/bootstrap.ps1
@@ -1,6 +1,22 @@
-$BinPath = Split-Path -Path $PSCommandPath -Parent
-& "$BinPath\InstallChocolatey.ps1"
+# Setup path variables
+$binPath = Split-Path -Path $PSCommandPath -Parent
+$scriptPath = Split-Path -Path $binPath -Parent
+$basePath = Split-Path -Path $scriptPath -Parent
+
+# Ensure Chocolatey is setup
+& "$binPath\InstallChocolatey.ps1"
 if ($LastExitCode) {
     Write-Host "Install/upgrade of Chocolatey failed with exit code: $LastExitCode"
     Exit $LastExitCode
+}
+
+# Install required packages
+$packagesFilePath = "$basePath\chocolatey-packages"
+if (Test-Path -Path "$packagesFilePath" -PathType Leaf) {
+    Write-Host "==> Installing Chocolatey packages..."
+    $packageList = Get-Content -Path "$packagesFilePath"
+    For ($index = 0; $index -lt $packageList.Count; $index++) {
+        $package = $packageList[$index];
+        Write-Host "===> Installing '$package'"
+    }
 }
