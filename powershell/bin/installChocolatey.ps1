8d897c6de611af5afbdb77d522cda2f51812c0d4
Pete O'Shea <mail@peteoshea.co.uk>
2020-06-02 20:00:45 +0100

    Change to use recommended process to install Chocolatey

--- powershell/bin/installChocolatey.ps1
+++ powershell/bin/installChocolatey.ps1
@@ -1,30 +1,16 @@
 # Check if Chocolatey is already installed
 $installed = $false
-if (Get-Command choco.exe -ErrorAction SilentlyContinue) {
+if (Get-Command choco -ErrorAction SilentlyContinue) {
     $installed = $true
 }
 if ($installed -eq $false) {
     Write-Host "==> Installing Chocolatey..."
 
-    # Download and save installation script
-    $url = 'https://chocolatey.org/install.ps1'
-    $outPath = "$env:temp\installChocolatey.ps1"
-    Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $outPath
-
-    # Test signature
-    $result = Get-AuthenticodeSignature -FilePath $outPath
-    if ($result.Status -ne 'Valid')
-    {
-        Write-Warning "Installation Script Damaged/Malware?"
-        Exit 1
-    }
-
-    # Run the installion script
-    & "$outPath"
-
-    # Tidy up
-    Remove-Item -Path $outPath
-} else {
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+else {
     Write-Host "==> Updating Chocolatey..."
     choco upgrade chocolatey
 }
