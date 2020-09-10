a6efedb0509d39c877cce9483e60577ceca46e6c
Pete O'Shea <mail@peteoshea.co.uk>
2020-09-10 18:12:06 +0100

    Allow for running of specific tests

--- checkScripts.ps1
+++ checkScripts.ps1
@@ -1,1 +1,18 @@
-Invoke-ScriptAnalyzer -Path .\script -Recurse
+param(
+    [Parameter(Mandatory=$false, Position=0, ValueFromRemainingArguments,
+    HelpMessage="Enter one or more specific tests")]
+    [string[]]
+    $Test
+)
+
+if ($PSBoundParameters.ContainsKey('Test')) {
+    Write-Output "Run specific tests"
+    for ($index = 0; $index -lt $Test.Count; $index++)
+    {
+       Write-Output "Test: $($Test[$index])"
+       Invoke-ScriptAnalyzer -Path "$($Test[$index])"
+    }
+} else {
+    Write-Output "Run all tests"
+    Invoke-ScriptAnalyzer -Path .\script -Recurse
+}
--- script/test.ps1
+++ script/test.ps1
@@ -1,10 +1,21 @@
+param(
+    [Parameter(Mandatory=$false, Position=0, ValueFromRemainingArguments,
+    HelpMessage="Enter one or more specific tests")]
+    [string[]]
+    $Test
+)
+
 $scriptPath = Split-Path -Path $PSCommandPath -Parent
 $basePath = Split-Path -Path $scriptPath -Parent
 
 Write-Output "`n==> Running tests..."
 Write-Output "Tests started at $(Get-Date)"
 
-& "$basePath\checkScripts.ps1"
+if ($PSBoundParameters.ContainsKey('Test')) {
+    & "$basePath\checkScripts.ps1" $Test
+} else {
+    & "$basePath\checkScripts.ps1"
+}
 if ($LastExitCode) {
     Write-Output "checkScripts failed with exit code: $LastExitCode"
     Exit $LastExitCode
