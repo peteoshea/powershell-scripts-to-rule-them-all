722136a6edb7f796911dd0335872406a273fd29d
Pete O'Shea <mail@peteoshea.co.uk>
2020-09-12 09:13:38 +0100

    Check output of script analyser and update exit code
    Show start and end time even if script fails.
    Test script also exits with exit code from check script so CI will fail accordingly.

--- script/bin/checkScripts.ps1
+++ script/bin/checkScripts.ps1
@@ -5,14 +5,28 @@ param(
     $Test
 )
 
+$errors = 0;
+$warnings = 0;
+$hints = 0;
+
 if ($PSBoundParameters.ContainsKey('Test')) {
     Write-Output "Lint specific scripts..."
     for ($index = 0; $index -lt $Test.Count; $index++)
     {
        Write-Output "Analysing: $($Test[$index])"
-       Invoke-ScriptAnalyzer -Path "$($Test[$index])" -ReportSummary
+       $results = Invoke-ScriptAnalyzer -Path "$($Test[$index])" -ReportSummary
+       $errors += $results.Where({$_.Severity -eq 'Error'}).Count
+       $warnings += $results.Where({$_.Severity -eq 'Warning'}).Count
+       $hints += $results.Where({$_.Severity -eq 'Information'}).Count
     }
 } else {
     Write-Output "Lint all scripts..."
-    Invoke-ScriptAnalyzer -Path .\script -Recurse -ReportSummary
+    $results = Invoke-ScriptAnalyzer -Path .\script -Recurse -ReportSummary
+    $errors += $results.Where({$_.Severity -eq 'Error'}).Count
+    $warnings += $results.Where({$_.Severity -eq 'Warning'}).Count
+    $hints += $results.Where({$_.Severity -eq 'Information'}).Count
 }
+
+# In this case even hints are considered a problem and will produce a non-zero exit code
+$exitCode = $errors + $warnings + $hints
+Exit $exitCode
--- script/test.ps1
+++ script/test.ps1
@@ -9,15 +9,14 @@ $scriptPath = Split-Path -Path $PSCommandPath -Parent
 
 Write-Output "`n==> Running tests..."
 Write-Output "Tests started at $(Get-Date)"
-
 if ($PSBoundParameters.ContainsKey('Test')) {
     & "$scriptPath\bin\checkScripts.ps1" $Test
 } else {
     & "$scriptPath\bin\checkScripts.ps1"
 }
+Write-Output "Tests finished at $(Get-Date)"
+
 if ($LastExitCode) {
     Write-Output "checkScripts failed with exit code: $LastExitCode"
     Exit $LastExitCode
 }
-
-Write-Output "Tests finished at $(Get-Date)"
